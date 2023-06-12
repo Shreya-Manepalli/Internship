f344c78326dee50d82e32fbed6e8ce280493b606
Shreya-Manepalli <114294836+Shreya-Manepalli@users.noreply.github.com>
2023-06-12 14:02:15 +0530

    Delete extract_tab_structured.py

--- extract_tab_structured.py
+++
@@ -1,7 +0,0 @@
-import tabula
-
-pdf_path = "https://sedl.org/afterschool/toolkits/science/pdf/ast_sci_data_tables_sample.pdf"
-
-dfs = tabula.read_pdf(pdf_path, pages='all')
-for i in range(len(dfs)):
-    dfs[i].to_csv(f"table_{i}.csv")
