53867f45da72fa40d27b9bd3950a2e4c5508f65b
Shreya-Manepalli <114294836+Shreya-Manepalli@users.noreply.github.com>
2023-06-30 13:46:07 +0530

    Add files via upload

--- ColorCodes/Code/image-2-chunks.py
+++ ColorCodes/Code/image-2-chunks.py
@@ -1,14 +1,15 @@
+'''
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None 
 def crop_image_into_chunks(image_path):
     image = Image.open(image_path)
     width, height = image.size
-    chunk_x = width // 3
-    chunk_y = height // 3
+    chunk_x = width // 12
+    chunk_y = height // 12
     padding = 380
     chunks = []
-    for i in range(3):  
-        for j in range(3): 
+    for i in range(12):  
+        for j in range(12): 
             left = j * chunk_x
             upper = i * chunk_y
             right = left + chunk_x
@@ -21,6 +22,149 @@ def crop_image_into_chunks(image_path):
             chunks.append(chunk)
     return chunks
 
+image_path = 'output_page_0.jpg'
+cropped_chunks = crop_image_into_chunks(image_path)
+for i, chunk in enumerate(cropped_chunks):
+    chunk.save(f'output/chunk_{i+1}.jpg')
+'''
+'''
+from PIL import Image
+import cv2
+import numpy as np
+
+Image.MAX_IMAGE_PIXELS = None
+
+def has_data(chunk):
+    chunk_gray = chunk.convert('L')
+    edges = cv2.Canny(np.array(chunk_gray), 50, 150)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    min_area = chunk.width * chunk.height * 0.001
+    max_area = chunk.width * chunk.height * 0.9
+    valid_contours = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        x, y, w, h = cv2.boundingRect(contour)
+        aspect_ratio = w / h if h != 0 else 0
+        if area > min_area and area < max_area and aspect_ratio > 0.1 and aspect_ratio < 10:
+            valid_contours.append(contour)
+    
+    if valid_contours:
+        return True
+    
+    return False
+
+def crop_image_into_chunks(image_path):
+    image = Image.open(image_path)
+    width, height = image.size
+    chunk_x = width // 15
+    chunk_y = height // 15
+    padding = 480
+    padding_top_bottom = 380
+    chunks = []
+    for i in range(14):
+        for j in range(14):
+            left = j * chunk_x
+            upper = i * chunk_y
+            right = left + chunk_x
+            lower = upper + chunk_y
+            left_padding = padding if j > 0 else 0
+            right_padding = padding if j < 2 else 0
+            top_padding = padding_top_bottom if i > 0 else 0
+            bottom_padding = padding_top_bottom if i < 2 else 0
+            left -= left_padding
+            right += right_padding
+            upper -= top_padding
+            lower += bottom_padding
+            chunk = image.crop((left, upper, right, lower))
+            if has_data(chunk):
+                chunks.append(chunk)
+    return chunks
+
+
+image_path = 'output_page_0.jpg'
+cropped_chunks = crop_image_into_chunks(image_path)
+for i, chunk in enumerate(cropped_chunks):
+    chunk.save(f'output/chunk_{i+1}.jpg')
+    '''
+
+from PIL import Image
+import cv2
+import numpy as np
+
+Image.MAX_IMAGE_PIXELS = None
+
+def detect_triangles(chunk):
+    # Convert the chunk to grayscale and apply Canny edge detection
+    chunk_gray = chunk.convert('L')
+    edges = cv2.Canny(np.array(chunk_gray), 50, 150)
+
+    # Find contours in the edge map
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Count the number of triangles
+    triangle_count = 0
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 0.03 * cv2.arcLength(contour, True), True)
+        if len(approx) == 3:
+            triangle_count += 1
+
+    return triangle_count
+
+def has_data(chunk):
+    # Convert the chunk to grayscale and apply Canny edge detection
+    chunk_gray = chunk.convert('L')
+    edges = cv2.Canny(np.array(chunk_gray), 50, 150)
+
+    # Find contours in the edge map
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Filter contours based on area and aspect ratio
+    min_area = chunk.width * chunk.height * 0.001
+    max_area = chunk.width * chunk.height * 0.9
+    valid_contours = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        x, y, w, h = cv2.boundingRect(contour)
+        aspect_ratio = w / h if h != 0 else 0
+        if area > min_area and area < max_area and aspect_ratio > 0.1 and aspect_ratio < 10:
+            valid_contours.append(contour)
+
+    if valid_contours:
+        return True
+
+    return False
+
+def crop_image_into_chunks(image_path):
+    image = Image.open(image_path)
+    width, height = image.size
+    chunk_x = width // 15
+    chunk_y = height // 15
+    padding = 480
+    padding_top_bottom = 380
+    chunks = []
+    triangle_counts = []
+    for i in range(14):
+        for j in range(14):
+            left = j * chunk_x
+            upper = i * chunk_y
+            right = left + chunk_x
+            lower = upper + chunk_y
+            left_padding = padding if j > 0 else 0
+            right_padding = padding if j < 2 else 0
+            top_padding = padding_top_bottom if i > 0 else 0
+            bottom_padding = padding_top_bottom if i < 2 else 0
+            left -= left_padding
+            right += right_padding
+            upper -= top_padding
+            lower += bottom_padding
+            chunk = image.crop((left, upper, right, lower))
+            if has_data(chunk):
+                chunks.append(chunk)
+                triangle_count = detect_triangles(chunk)
+                triangle_counts.append(triangle_count)
+    return chunks, triangle_counts
 image_path = 'output_page_0.jpg'
 cropped_chunks = crop_image_into_chunks(image_path)
 for i, chunk in enumerate(cropped_chunks):
--- ColorCodes/Code/text_format.py
+++ ColorCodes/Code/text_format.py
@@ -1,3 +1,4 @@
+'''
 import easyocr
 import os
 import re
@@ -26,5 +27,180 @@ for file in files:
                     color = None
 
 formatted_output = ', '.join([f"{color}: {'-'.join(numbers)}" for color, numbers in color_number_pairs])
+print(formatted_output)
+
+'''
+'''
+import easyocr
+import os
+import re
+import cv2
+
+def count_triangles(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    triangle_count = 0
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+        if len(approx) == 3:
+            triangle_count += 1
+
+    return triangle_count
+
+model_path = 'new_model.pth'
+reader = easyocr.Reader(['en'], model_storage_directory=model_path)
+folder_path = 'output'
+files = os.listdir(folder_path)
+
+colors_to_extract = ['GREEN', 'PINK', 'PURPLE', 'BLUE']
+color_number_chunks = []
+current_chunk = []
+
+for file in files:
+    if file.endswith(('.jpg', '.png', '.jpeg', '.tiff')):
+        image_path = os.path.join(folder_path, file)
+        image = cv2.imread(image_path)
+        result = reader.readtext(image_path, detail=0)
+
+        for text in result:
+            if re.match(r'^P(?!URPLE)', text):
+                color = 'PINK'
+            elif text.isalpha() and text in colors_to_extract:
+                color = text
+            elif re.match(r'^\d{1,3}(?:\s*-?\s*\d{1,3})*$', text):
+                numbers = text.split('-')
+                if color:
+                    current_chunk.append((color, numbers))
+                    color = None
+        triangle_count = count_triangles(image)
+        if current_chunk:
+            color_number_chunks.append((current_chunk, triangle_count))
+            current_chunk = []
+
+for chunk, triangle_count in color_number_chunks:
+    print("Chunk:")
+    for color, numbers in chunk:
+        print(f"Color: {color}, Number: {'-'.join(numbers)}")
+    print(f"Number of triangles: {triangle_count}")
+    print()
+
+'''
+'''
+import easyocr
+import os
+import re
+
+model_path = 'new_model.pth'
+reader = easyocr.Reader(['en'], model_storage_directory=model_path)
+folder_path = 'output'
+files = os.listdir(folder_path)
+
+color_number_pairs = []
+colors_to_extract = ['GREEN', 'PINK','PURPLE', 'BLUE']
+color = None  # Initialize color variable outside the loop
+
+for file in files:
+    if file.endswith(('.jpg', '.png', '.jpeg', '.tiff')):
+        image_path = os.path.join(folder_path, file)
+        result = reader.readtext(image_path, detail=0)
+
+        for text in result:
+            if re.match(r'^P(?!URPLE)', text):
+                color = 'PINK'
+            elif text.isalpha() and text in colors_to_extract:
+                if color:
+                    color_number_pairs.append((color, numbers))
+                color = text
+                numbers = []  # Initialize numbers list
+            elif re.match(r'^\d{1,3}(?:\s*-?\s*\d{1,3})*$', text):
+                numbers += text.split('-')
+
+        # Append the last color and numbers if any
+        if color:
+            color_number_pairs.append((color, numbers))
+
+formatted_output = ', '.join([f"{color}: {'-'.join(numbers)}" for color, numbers in color_number_pairs])
+print(formatted_output)
+'''
+'''
+import easyocr
+import os
+import re
+
+model_path = 'new_model.pth'
+reader = easyocr.Reader(['en'], model_storage_directory=model_path)
+folder_path = 'output'
+files = os.listdir(folder_path)
+
+color_number_pairs = []
+colors_to_extract = ['GREEN', 'PINK','PURPLE', 'BLUE']
+color = None  # Initialize color variable outside the loop
+
+for file in files:
+    if file.endswith(('.jpg', '.png', '.jpeg', '.tiff')):
+        image_path = os.path.join(folder_path, file)
+        result = reader.readtext(image_path, detail=0)
+
+        for text in result:
+            if re.match(r'^P(?!URPLE)', text):
+                color = 'PINK'
+            elif text.isalpha() and text in colors_to_extract:
+                if color:
+                    color_number_pairs.append((color, numbers))
+                color = text
+                numbers = []  # Initialize numbers list
+            elif re.match(r'^\d{1,3}(?:\s*-?\s*\d{1,3})*$', text):
+                if numbers:
+                    color_number_pairs.append((color, numbers))
+                numbers = text.split('-')
+
+        # Append the last color and numbers if any
+        if color and numbers:
+            color_number_pairs.append((color, numbers))
+
+formatted_output = ', '.join([f"{color}: {'-'.join(numbers)}" for color, numbers in color_number_pairs])
+print(formatted_output)
+'''
+import easyocr
+import os
+import re
+
+model_path = 'new_model.pth'
+reader = easyocr.Reader(['en'], model_storage_directory=model_path)
+folder_path = 'output'
+files = os.listdir(folder_path)
+
+color_number_pairs = []
+colors_to_extract = ['GREEN', 'PINK', 'PURPLE', 'BLUE']
+color = None  # Initialize color variable outside the loop
+
+for file in files:
+    if file.endswith(('.jpg', '.png', '.jpeg', '.tiff')):
+        image_path = os.path.join(folder_path, file)
+        result = reader.readtext(image_path, detail=0)
+
+        for text in result:
+            if re.match(r'^P(?!URPLE)', text):
+                color = 'PINK'
+            elif text.isalpha() and text in colors_to_extract:
+                if color and numbers:
+                    color_number_pairs.append((color, tuple(numbers)))
+                color = text
+                numbers = []  # Initialize numbers list
+            elif re.match(r'^\d{1,3}(?:\s*-\s*\d{1,3})*$', text):
+                if numbers:
+                    color_number_pairs.append((color, tuple(numbers)))
+                numbers = text.split('-')
+
+        # Append the last color and numbers if any
+        if color and numbers:
+            color_number_pairs.append((color, tuple(numbers)))
+
+# Remove redundant color-number pairs
+unique_pairs = set(color_number_pairs)
+
+formatted_output = ', '.join([f"{color}: {'-'.join(numbers)}" for color, numbers in unique_pairs])
 print(formatted_output)
 
