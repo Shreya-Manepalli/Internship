0957fe88afed3757a7f91837ebdf834cd4554b8d
Shreya-Manepalli <114294836+Shreya-Manepalli@users.noreply.github.com>
2023-07-12 16:16:11 +0530

    Add files via upload

---
+++ description_format.py
@@ -0,0 +1,20 @@
+import re
+
+def extract_values(statement):
+    match = re.search(r"((\d+\.\d+)\sor\s(\d+\.\d+))", statement)
+    if match:
+        measured_value = float(match.group(2))
+        deviation_value = float(match.group(3))
+        return  measured_value, deviation_value
+    else:
+        return None
+
+# Example usage
+statement = "True Position |0.1|A|B|C| is  0.154 or 0.054 out of tolerance Please see attached 44DAC02057 v2  support pack  for details."
+values = extract_values(statement)
+if values:
+    measured_value, deviation_value = values
+    print("Measured Value:", measured_value)
+    print("Deviation Value:", deviation_value)
+else:
+    print("No match found.")
